6e892caa1aeb708cf3a4bc0c94b7657cdda5f650
Skyler Mayfield <skyler544@gmail.com>
2025-02-08 15:57:16 +0100

    add and remove more packages

--- build.sh
+++ build.sh
@@ -3,7 +3,21 @@
 set -ouex pipefail
 
 ### Configure packages
-rpm-ostree override remove firefox firefox-langpacks \
-           && rpm-ostree install mg
+rpm-ostree override remove \
+           firefox \
+           firefox-langpacks \
+           gnome-tour \
+    && \
+    rpm-ostree install \
+               docker-buildx-plugin \
+               docker-ce \
+               docker-ce-cli \
+               docker-compose-plugin \
+               input-remapper \
+               google-noto-fonts-all \
+               mg \
+               podman-compose
 
+systemctl enable docker.socket
+systemctl enable input-remapper
 systemctl enable podman.socket
