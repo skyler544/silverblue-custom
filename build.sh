c85ece902e4a5b82aa7ff070d269da33f4f66cb4
Skyler Mayfield <mayfield@fynk.com>
2025-02-10 10:22:24 +0100

    rebase to bluefin and remove yaru-icons

--- build.sh
+++ build.sh
@@ -4,16 +4,8 @@ set -ouex pipefail
 
 ### Configure packages
 rpm-ostree override remove \
-           firefox \
-           firefox-langpacks \
            gnome-tour \
+           yaru-theme \
     && \
     rpm-ostree install \
-               input-remapper \
-               google-noto-fonts-all \
-               mg \
-               podman-docker \
-               podman-compose
-
-systemctl enable input-remapper
-systemctl enable podman.socket
+               mg
