6df932ee287ae496c9f7f3d8709cd0c1c17cc7b0
Skyler Mayfield <skyler544@gmail.com>
2025-02-10 20:09:55 +0100

    how many of these yaru packages can one man remove?

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ set -ouex pipefail
 rpm-ostree override remove \
            gnome-tour \
            yaru-theme \
+           yaru-gtk2-theme \
+           yaru-sound-theme \
+           yaru-icon-theme \
+           yaru-gtksourceview-theme \
+           yaru-gtk4-theme \
+           yaru-gtk3-theme \
            gnome-shell-theme-yaru \
     && \
     rpm-ostree install \
