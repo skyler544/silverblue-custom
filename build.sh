2dcadabe220f40c32e8422c845e8655131b1ae42
Skyler Mayfield <skyler544@gmail.com>
2025-02-12 21:26:24 +0100

    does package order matter? if not, why not alphabetical

--- build.sh
+++ build.sh
@@ -7,24 +7,24 @@ dnf -y copr enable alternateved/bleeding-emacs
 ### Configure packages
 rpm-ostree override remove \
            default-fonts-core-emoji \
-           google-noto-color-emoji-fonts \
-           gnome-tour \
-           yaru-theme \
-           yaru-gtk2-theme \
-           yaru-sound-theme \
-           yaru-icon-theme \
-           yaru-gtksourceview-theme \
-           yaru-gtk4-theme \
-           yaru-gtk3-theme \
            gnome-shell-theme-yaru \
+           gnome-tour \
+           google-noto-color-emoji-fonts \
+           yaru-gtk2-theme \
+           yaru-gtk3-theme \
+           yaru-gtk4-theme \
+           yaru-gtksourceview-theme \
+           yaru-icon-theme \
+           yaru-sound-theme \
+           yaru-theme \
     && \
     rpm-ostree install \
-               papirus-icon-theme \
-               papirus-icon-theme-light \
-               papirus-icon-theme-dark \
-               wl-clipboard \
-               google-noto-emoji-fonts \
                emacs \
-               mg
+               google-noto-emoji-fonts \
+               mg \
+               papirus-icon-theme \
+               papirus-icon-theme-dark \
+               papirus-icon-theme-light \
+               wl-clipboard
 
 dnf -y copr disable alternateved/bleeding-emacs
