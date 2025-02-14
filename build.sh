d6ac139104a35ad27de4e3a136066aae17a36f80
Skyler Mayfield <mayfield@fynk.com>
2025-02-14 09:25:24 +0100

    put packages into arrays

--- build.sh
+++ build.sh
@@ -2,31 +2,37 @@
 
 set -ouex pipefail
 
+
+EXCLUDED_PACKAGES=(
+    cascadia-code-fonts
+    default-fonts-core-emoji
+    gnome-shell-theme-yaru
+    gnome-tour
+    google-noto-color-emoji-fonts
+    solaar
+    yaru-gtk2-theme
+    yaru-gtk3-theme
+    yaru-gtk4-theme
+    yaru-gtksourceview-theme
+    yaru-icon-theme
+    yaru-sound-theme
+    yaru-theme
+)
+
+INCLUDED_PACKAGES=(
+    emacs
+    google-noto-emoji-fonts
+    mg
+    papirus-icon-theme
+    papirus-icon-theme-dark
+    papirus-icon-theme-light
+    wl-clipboard
+)
+
+
 dnf -y copr enable alternateved/bleeding-emacs
 
-### Configure packages
-rpm-ostree override remove \
-           cascadia-code-fonts \
-           default-fonts-core-emoji \
-           gnome-shell-theme-yaru \
-           gnome-tour \
-           google-noto-color-emoji-fonts \
-           solaar \
-           yaru-gtk2-theme \
-           yaru-gtk3-theme \
-           yaru-gtk4-theme \
-           yaru-gtksourceview-theme \
-           yaru-icon-theme \
-           yaru-sound-theme \
-           yaru-theme \
-    && \
-    rpm-ostree install \
-               emacs \
-               google-noto-emoji-fonts \
-               mg \
-               papirus-icon-theme \
-               papirus-icon-theme-dark \
-               papirus-icon-theme-light \
-               wl-clipboard
+rpm-ostree override remove "${EXCLUDED_PACKAGES[@]}"
+rpm-ostree install "${INCLUDED_PACKAGES[@]}"
 
 dnf -y copr disable alternateved/bleeding-emacs
