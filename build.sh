e9bdefc035af5ba8f3e8affac7a4cb53a2ca0709
Skyler Mayfield <mayfield@fynk.com>
2025-02-17 14:15:30 +0100

    remove more fonts

--- build.sh
+++ build.sh
@@ -6,12 +6,19 @@ set -ouex pipefail
 # PACKAGES
 # ----------------------------------------------------
 EXCLUDED_PACKAGES=(
+    adobe-source-code-pro-fonts
     cascadia-code-fonts
     default-fonts-core-emoji
     gnome-shell-theme-yaru
     gnome-tour
+    google-go-mono-fonts
     google-noto-color-emoji-fonts
+    ibm-plex-mono-fonts
+    mozilla-fira-mono-fonts
+    nerd-fonts
+    opendyslexic-fonts
     solaar
+    ubuntu-family-fonts
     yaru-gtk2-theme
     yaru-gtk3-theme
     yaru-gtk4-theme
