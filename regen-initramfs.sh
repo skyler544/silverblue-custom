494ac79681742aef228dc9fdca710dc935af655d
Skyler Mayfield <skyler544@gmail.com>
2025-09-28 18:12:25 +0200

    Test if rpm-ostree initramfs is enough for this config to work

--- regen-initramfs.sh
+++ regen-initramfs.sh
@@ -3,7 +3,7 @@ set -ouex pipefail
 
 KVER="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 OUTPUT_IMG="/lib/modules/$KVER/initramfs.img"
-MODULES="ostree bluetooth"
+MODULES="ostree"
 
 dracut --force \
        --verbose \
