a2da7d5d6c1c818e58da4828be381e7fd3aa9d62
Skyler Mayfield <skyler544@gmail.com>
2025-09-28 16:52:26 +0200

    Add bluetooth support to initrd

--- regen-initramfs.sh
+++ regen-initramfs.sh
@@ -3,7 +3,7 @@ set -ouex pipefail
 
 KVER="$(rpm -qa | grep -P 'kernel-(\d+\.\d+\.\d+)' | sed -E 's/kernel-//')"
 OUTPUT_IMG="/lib/modules/$KVER/initramfs.img"
-MODULES="ostree"
+MODULES="ostree bluetooth"
 
 dracut --force \
        --no-hostonly \
